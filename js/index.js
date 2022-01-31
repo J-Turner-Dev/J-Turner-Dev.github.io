78617c4b174aee7dd442b85e01a51bf2089c73e3
J-Turner-Dev <jturner.dev@gmail.com>
2022-01-31 13:18:15 -0600

    First Commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+    document.addEventListener('scroll', () => {
+        document.body.classList.remove('nav-open');
+    });
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
